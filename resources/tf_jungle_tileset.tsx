55508b6eb3d483253a438cf286e521507eb7d01d
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2023-02-24 16:17:50 -0300

    Update jungle map

--- resources/tf_jungle_tileset.tsx
+++ resources/tf_jungle_tileset.tsx
@@ -1,4 +1,484 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="tf_jungle_tileset" tilewidth="16" tileheight="16" tilecount="462" columns="22">
  <image source="../assets/img/tf_jungle_tileset.png" width="352" height="336"/>
+ <tile id="177">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="178">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="179">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="187">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="188">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="189">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="192">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="193">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="194">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="199">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="201">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="204">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="205">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="206">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="209">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="210">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="211">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="214">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="215">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="216">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="221">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="223">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="226">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="227">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="228">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="231">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="232">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="233">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="236">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="237">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="238">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="243">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="244">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="245">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="253">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="254">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="255">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="258">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="259">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="260">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="265">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="266">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="267">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="268">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="269">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="270">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="271">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="272">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="275">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="276">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="277">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="280">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="281">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="282">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="287">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="288">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="289">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="290">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="291">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="292">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="293">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="294">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="309">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="310">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="311">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="312">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="313">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="314">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="315">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="316">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="384">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="385">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="386">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="387">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="388">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="389">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="390">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="391">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="392">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="393">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="406">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="407">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="408">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="409">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="410">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="411">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="412">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="413">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="414">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="415">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="430">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="431">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="432">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="433">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="434">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
+ <tile id="435">
+  <properties>
+   <property name="collides" value="1"/>
+  </properties>
+ </tile>
 </tileset>
