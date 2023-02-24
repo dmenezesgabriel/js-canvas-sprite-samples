a37fde8b492946e546ed528b27924111d82d5e53
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2023-02-24 20:25:35 -0300

    Add game class

---
+++ assets/js/game.js
@@ -0,0 +1,112 @@
+import Player from "./player.js";
+import Camera from "./Camera.js";
+import Display from "./display.js";
+
+export default class Game {
+  constructor() {
+    this.canvas = document.getElementById("canvas1");
+    this.canvas.width = 500;
+    this.canvas.height = 500;
+    this.display = new Display(this.canvas);
+    this.keys = [];
+  }
+  async create() {
+    const moveKeys = [
+      "w",
+      "a",
+      "s",
+      "d",
+      "ArrowUp",
+      "ArrowDown",
+      "ArrowLeft",
+      "ArrowRight",
+    ];
+
+    const getAnimationStates = await fetch(
+      "resources/knights-animation-states.json"
+    );
+    const animationStates = await getAnimationStates.json();
+
+    const currentAnimationStates = animationStates["golden-knight"];
+
+    const playerSpriteImg = new Image();
+    playerSpriteImg.src = currentAnimationStates["img"];
+    const backgroundImg = new Image();
+    backgroundImg.src = "assets/img/background.jpg";
+
+    this.background = {
+      img: backgroundImg,
+      x: 0,
+      y: 0,
+      width: 1280,
+      height: 720,
+    };
+
+    this.player = new Player(
+      playerSpriteImg,
+      currentAnimationStates,
+      this.background,
+      this.canvas.width / 2 - currentAnimationStates["width"] / 2,
+      this.canvas.height / 2 - currentAnimationStates["height"] / 2,
+      currentAnimationStates["width"],
+      currentAnimationStates["height"],
+      currentAnimationStates["initialXFrame"],
+      currentAnimationStates["initialYFrame"],
+      10,
+      false
+    );
+
+    this.camera = new Camera(
+      this.player,
+      this.background,
+      0,
+      0,
+      this.canvas.width,
+      this.canvas.height
+    );
+
+    this.camera.x = (this.background.width - this.camera.width) / 2;
+    this.camera.y = (this.background.height - this.camera.height) / 2;
+    this.player.x = (this.background.width - this.camera.width) / 2;
+    this.player.y = (this.background.height - this.camera.height) / 2;
+
+    window.addEventListener("keydown", (e) => {
+      this.keys[e.key] = true;
+      if (moveKeys.includes(e.key)) {
+        this.player.moving = true;
+      }
+    });
+
+    window.addEventListener("keyup", (e) => {
+      delete this.keys[e.key];
+      if (moveKeys.includes(e.key)) {
+        this.player.moving = false;
+      }
+    });
+  }
+
+  update() {
+    const sprites = [];
+    sprites.push({
+      image: this.background.img,
+      sourceX: 0,
+      sourceY: 0,
+      destinationX: 0,
+      destinationY: 0,
+      width: this.background.width,
+      height: this.background.height,
+    });
+    sprites.push({
+      image: this.player.img,
+      sourceX: this.player.width * this.player.frameX,
+      sourceY: this.player.height * this.player.frameY,
+      destinationX: this.player.x,
+      destinationY: this.player.y,
+      width: this.player.width,
+      height: this.player.height,
+    });
+    this.display.render(sprites, this.camera);
+    this.player.update(this.keys);
+    this.camera.update();
+  }
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,108 +1,7 @@
-import Player from "./player.js";
-import Camera from "./Camera.js";
-import Display from "./display.js";
+import Game from "./game.js";
 
-const canvas = document.getElementById("canvas1");
-const CANVAS_WIDTH = (canvas.width = 500);
-const CANVAS_HEIGHT = (canvas.height = 500);
-
-const keys = [];
-const moveKeys = [
-  "w",
-  "a",
-  "s",
-  "d",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-];
-
-const getAnimationStates = await fetch(
-  "resources/knights-animation-states.json"
-);
-const animationStates = await getAnimationStates.json();
-
-const currentAnimationStates = animationStates["golden-knight"];
-
-const playerSpriteImg = new Image();
-playerSpriteImg.src = currentAnimationStates["img"];
-const backgroundImg = new Image();
-backgroundImg.src = "assets/img/background.jpg";
-
-let fps, fpsInterval, startTime, now, then, elapsed;
-
-const display = new Display(canvas);
-
-const background = {
-  img: backgroundImg,
-  x: 0,
-  y: 0,
-  width: 1280,
-  height: 720,
-};
-
-const player = new Player(
-  playerSpriteImg,
-  currentAnimationStates,
-  background,
-  canvas.width / 2 - currentAnimationStates["width"] / 2,
-  canvas.height / 2 - currentAnimationStates["height"] / 2,
-  currentAnimationStates["width"],
-  currentAnimationStates["height"],
-  currentAnimationStates["initialXFrame"],
-  currentAnimationStates["initialYFrame"],
-  10,
-  false
-);
-
-const camera = new Camera(
-  player,
-  background,
-  0,
-  0,
-  canvas.width,
-  canvas.height
-);
-
-window.addEventListener("keydown", function (e) {
-  keys[e.key] = true;
-  if (moveKeys.includes(e.key)) {
-    player.moving = true;
-  }
-});
-
-window.addEventListener("keyup", function (e) {
-  delete keys[e.key];
-  if (moveKeys.includes(e.key)) {
-    player.moving = false;
-  }
-});
-
-function update() {
-  const sprites = [];
-  sprites.push({
-    image: background.img,
-    sourceX: 0,
-    sourceY: 0,
-    destinationX: 0,
-    destinationY: 0,
-    width: background.width,
-    height: background.height,
-  });
-  sprites.push({
-    image: player.img,
-    sourceX: player.width * player.frameX,
-    sourceY: player.height * player.frameY,
-    destinationX: player.x,
-    destinationY: player.y,
-    width: player.width,
-    height: player.height,
-  });
-  display.render(sprites, camera);
-  player.update(keys);
-  camera.update();
-}
+let fpsInterval, startTime, now, then, elapsed;
+const game = new Game();
 
 function gameLoop() {
   window.requestAnimationFrame(gameLoop);
@@ -110,16 +9,13 @@ function gameLoop() {
   elapsed = now - then;
   if (elapsed > fpsInterval) {
     then = now - (elapsed % fpsInterval);
-    update();
+    game.update();
   }
 }
 
-function startGame(fps) {
+async function startGame(fps) {
   // center camera
-  camera.x = (background.width - camera.width) / 2;
-  camera.y = (background.height - camera.height) / 2;
-  player.x = (background.width - camera.width) / 2;
-  player.y = (background.height - camera.height) / 2;
+  await game.create();
 
   fpsInterval = 1000 / fps; // calculate milliseconds
   then = Date.now();
@@ -127,4 +23,4 @@ function startGame(fps) {
   gameLoop();
 }
 
-startGame(10);
+await startGame(10);
