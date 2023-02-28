61655a604c81087cf40d1ecd83964f07196dbf3f
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2023-02-28 13:11:24 -0300

    Update collision wip

--- assets/js/collision.js
+++ assets/js/collision.js
@@ -1,3 +1,29 @@
+function collides(
+  colReference,
+  rowReference,
+  mapData,
+  tileSetProperties,
+  tileSize,
+  tileScaleSize
+) {
+  const currentMapCol = Math.floor(colReference / (tileSize * tileScaleSize));
+  const currentMapRow = Math.floor(rowReference / (tileSize * tileScaleSize));
+
+  const tileIds = [];
+  for (const layer of mapData.layers) {
+    const colNumber = layer["width"];
+    const dataIndex = currentMapRow * colNumber + currentMapCol;
+    const data = layer["data"];
+    tileIds.push(data[dataIndex]);
+  }
+
+  const currentTiles = tileSetProperties["tiles"].filter((tile) =>
+    tileIds.includes(tile.id)
+  );
+
+  return currentTiles.some((tile) => (tile.properties.collide = true));
+}
+
 function mapCollides(
   objectX,
   objectY,
@@ -8,26 +34,33 @@ function mapCollides(
   tileSize,
   tileScaleSize
 ) {
-  //  + player.width = player end
-  const currentMapCol = Math.floor(
-    (objectX + objectWidth) / (tileSize * tileScaleSize)
-  );
-  //  + player.height = player bottom
-  const currentMapRow = Math.floor(
-    (objectY + objectHeight) / (tileSize * tileScaleSize)
-  );
+  const objectTop = objectY;
+  const objectLeft = objectX;
+  const objectRight = objectX + objectWidth;
+  const objectBottom = objectY + objectHeight;
 
-  const tileIds = [];
-  for (const layer of mapData.layers) {
-    const colNumber = layer["width"];
-    const dataIndex = currentMapRow * colNumber + currentMapCol;
-    const data = layer["data"];
-    tileIds.push(data[dataIndex]);
+  const combinations = [
+    { col: objectLeft, row: objectTop },
+    { col: objectRight, row: objectTop },
+    { col: objectLeft, row: objectBottom },
+    { col: objectRight, row: objectBottom },
+  ];
+
+  const hasCollision = [];
+
+  for (const combination of combinations) {
+    const itCollides = collides(
+      combination.col,
+      combination.row,
+      mapData,
+      tileSetProperties,
+      tileSize,
+      tileScaleSize
+    );
+    hasCollision.push(itCollides);
   }
-  const currentTiles = tileSetProperties["tiles"].filter((tile) =>
-    tileIds.includes(tile.id)
-  );
-  return currentTiles.some((tile) => (tile.properties.collide = true));
+
+  return hasCollision.some((value) => value === true);
 }
 
 export { mapCollides };
